cfc96aa81ce312c556d1349256ade86bdc365ced
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2026-04-20 20:47:03 -0500

    fix: corrigiendo YAML format en SFN.

--- cdk/lib/constructs/sfn/pipeline-state-machine.construct.ts
+++ cdk/lib/constructs/sfn/pipeline-state-machine.construct.ts
@@ -2,7 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import * as fs from 'fs';
 import * as path from 'path';
+import * as yaml from 'js-yaml';
 import { ResourceConstants } from '../../../common/constants/resource.constants';
 import { SfnRoleConstruct } from '../iam/sfn-role.construct';
 
@@ -26,18 +28,29 @@ export class PipelineStateMachineConstruct extends Construct {
       jobsTableArn:   props.jobsTable.tableArn,
     });
 
+    const aslDefinition = this.loadAsl(path.join(__dirname, 'pipeline.asl.yaml'), {
+      ParserFunctionArn:    props.parserFnArn,
+      ValidatorFunctionArn: props.validatorFnArn,
+      LoaderFunctionArn:    props.loaderFnArn,
+      JobsTableName:        props.jobsTable.tableName,
+    });
+
     this.stateMachine = new sfn.StateMachine(this, 'StateMachine', {
       stateMachineName: ResourceConstants.STATE_MACHINE,
       stateMachineType: sfn.StateMachineType.STANDARD,
-      definitionBody:   sfn.DefinitionBody.fromFile(path.join(__dirname, 'pipeline.asl.yaml')),
-      definitionSubstitutions: {
-        ParserFunctionArn:    props.parserFnArn,
-        ValidatorFunctionArn: props.validatorFnArn,
-        LoaderFunctionArn:    props.loaderFnArn,
-        JobsTableName:        props.jobsTable.tableName,
-      },
-      role:    role.role,
-      timeout: cdk.Duration.hours(1),
+      definitionBody:   sfn.DefinitionBody.fromString(aslDefinition),
+      role:             role.role,
+      timeout:          cdk.Duration.hours(1),
     });
+  }
+
+  private loadAsl(filePath: string, substitutions: Record<string, string>): string {
+    let raw = fs.readFileSync(filePath, 'utf-8');
+
+    for (const [key, value] of Object.entries(substitutions)) {
+      raw = raw.split(`\${${key}}`).join(value);
+    }
+
+    return JSON.stringify(yaml.load(raw));
   }
 }
